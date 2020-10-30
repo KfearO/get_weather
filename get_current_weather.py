0df63e073b84503ac7e051a8cd9348a203f598e3
Kfear Oshri <kfear.ok@gmail.com>
2020-10-30 02:08:28 +0200

    Added an option to print weather with units signs. Refactor get_current_weather.py

--- get_current_weather.py
+++ get_current_weather.py
@@ -2,6 +2,7 @@ import json
 from get_weather_api import get_weather_api
 from get_weather_classes import GetWeatherException, Location
 import concurrent.futures
+
 """
 A Short demonstration program to showcase how to use the get_weather_api()
 Notice the differences in each function call
@@ -10,21 +11,37 @@ a public demo file exist in the project - 'config.json'
 """
 
 
-def get_current_weather(check_location, private_api_key, units=None):
-    current_weather, check_location = get_weather_api(check_location, private_api_key, units)
-    print(check_location + "\n" + current_weather + "\n")
+def run_get_weather_api(locations_list):
+    results_list = []
+    get_current_weather_thread = concurrent.futures.ThreadPoolExecutor(locations_list.__len__())
+    for each_location in locations_list:
+        if each_location.__len__() == 2:
+            each_location.append(None)
+        results_list.append(get_current_weather_thread.submit(get_weather_api,
+                                                              each_location[0], each_location[1], each_location[2]))
+    return results_list
 
 
 if __name__ == "__main__":
-    try:
-        with open("my_config.json", "r") as c:
-            api_key = json.load(c)["application_parameters"]["api_key"]
-            get_current_weather_thread = concurrent.futures.ThreadPoolExecutor(4)
-            get_current_weather_thread.submit(get_current_weather, Location("New York"), api_key, "imperial")
-            get_current_weather_thread.submit(get_current_weather, Location("London", "Great Britain"), api_key)
-            get_current_weather_thread.submit(get_current_weather, Location("paris", "fr"), api_key, "metric")
-            get_current_weather_thread.submit(get_current_weather, Location("Tel-Aviv", "IL"), api_key)
+    with open("my_config.json", "r") as c:
+        api_key = json.load(c)["application_parameters"]["api_key"]
 
-    except GetWeatherException as e:
-        print(e.__str__() + "\n")
-        raise e
+    check_locations_list = [
+        [Location("New York"), api_key, "imperial"],
+        [Location("London", "Great Britain"), api_key],
+        [Location("paris", "fr"), api_key, "metric"],
+        [Location("Tel-Aviv", "IL"), api_key, "metric"]
+                       ]
+
+    threading_results = run_get_weather_api(check_locations_list)
+    concurrent.futures.wait(threading_results)
+    for weather_results in threading_results:
+        try:
+            result_current_weather, result_location = weather_results.result()
+            units = None
+            for location in check_locations_list:
+                if location[0].city == result_location.city and location.__len__() == 3:
+                    units = location[2]
+            print(result_location + "\n" + result_current_weather.string_with_units(units) + "\n")
+        except GetWeatherException as e:
+            print(e + "\n")
--- get_weather_classes.py
+++ get_weather_classes.py
@@ -33,24 +33,52 @@ class Location:
 
 class Weather:
     def __init__(self, humidity, air_temperature, air_pressure,
-                 wind_speed, wind_direction, cloud_cover, precipitation):
+                 wind_speed, wind_direction, cloud_cover, precipitations):
         self.humidity = humidity
         self.air_temperature = air_temperature
         self.air_pressure = air_pressure
         self.wind_speed = wind_speed
         self.wind_direction = wind_direction
         self.cloud_cover = cloud_cover
-        self.precipitation = precipitation
+        self.precipitations = precipitations
 
     def __str__(self):
         return \
-            "Temperature: " + str(self.air_temperature) + "\n" + \
-            "Humidity: " + str(self.humidity) + "\n" + \
-            "Air Pressure: " + str(self.air_pressure) + "\n" + \
-            "Wind Speed: " + str(self.wind_speed) + "\n" + \
-            "Wind Direction: " + str(self.wind_direction) + "\n" + \
-            "Cloud Cover: " + str(self.cloud_cover) + "\n" + \
-            "Precipitation: " + str(self.precipitation)
+            "Temperature: " + str(self.air_temperature) + ", " + \
+            "Humidity: " + str(self.humidity) + ", " + \
+            "Air Pressure: " + str(self.air_pressure) + ", " + \
+            "Wind Speed: " + str(self.wind_speed) + ", " + \
+            "Wind Direction: " + str(self.wind_direction) + ", " + \
+            "Cloud Cover: " + str(self.cloud_cover) + ", " + \
+            "Precipitations: " + str(self.precipitations)
+
+    def string_with_units(self, units=None):
+        if units is None:
+            return self.__str__()
+
+        if units.lower() == "metric":
+            air_temperature_unit = " C, "
+            wind_speed_unit = " Meters/Seconds, "
+        elif units.lower() == "imperial":
+            air_temperature_unit = " F, "
+            wind_speed_unit = " Miles/Hour, "
+        else:
+            raise GetWeatherException(21, "units can be either 'metric' or 'imperial'")
+
+        humidity_unit = " %, "
+        air_pressure_unit = " mb, "
+        wind_direction_unit = " deg. "
+        cloud_cover_unit = " %, "
+        precipitations = " mm/h"
+
+        return \
+            "Temperature: " + str(self.air_temperature) + air_temperature_unit + \
+            "Humidity: " + str(self.humidity) + humidity_unit + \
+            "Air Pressure: " + str(self.air_pressure) + air_pressure_unit + \
+            "Wind Speed: " + str(self.wind_speed) + wind_speed_unit + \
+            "Wind Direction: " + str(self.wind_direction) + wind_direction_unit + \
+            "Cloud Cover: " + str(self.cloud_cover) + cloud_cover_unit + \
+            "Precipitations: " + str(self.precipitations) + precipitations
 
     def __add__(self, other):
         return str(self) + str(other)
