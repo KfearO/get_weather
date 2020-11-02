65e6e67b06c35208e0eeb806a892bf48806745aa
Kfear Oshri <kfear.ok@gmail.com>
2020-11-02 14:13:55 +0200

    log is separated form get_weather_api.py to get_weather_logger.py

--- get_weather_api.py
+++ get_weather_api.py
@@ -1,42 +1,9 @@
 from get_weather_classes import *
-import tempfile
-import os.path
 import json
 import urllib.request
 import urllib.parse
-import logging
-from logging.handlers import RotatingFileHandler
 from cryptography.fernet import Fernet
-
-try:
-    with open("my_config.json", "r") as config_params:
-        config_json = json.load(config_params)
-    log_level = config_json["log_parameters"]["log_level"]
-    log_size_KB = config_json["log_parameters"]["log_size_KB"]
-    log_file_path = config_json["log_parameters"]["log_file_path"]
-    log_file_name = config_json["log_parameters"]["log_file_name"]
-    backup_logs = config_json["log_parameters"]["backup_logs"]
-    if not os.path.isdir(log_file_path):
-        raise GetWeatherException(10, "No such directory: '" + log_file_path + "' check your config file")
-except FileNotFoundError or KeyError as err:
-    if err is KeyError:
-        raise err
-    log_level = "INFO"
-    log_size_KB = 23
-    log_file_path = tempfile.gettempdir()
-    log_file_name = "temp_get_weather.log"
-    backup_logs = 9
-
-log_format = logging.Formatter('%(asctime)s.%(msecs)03d|%(levelname)s|%(threadName)s|%(funcName)s()|%(message)s',
-                               '%d/%m/%Y %H:%M:%S')
-log_handler = RotatingFileHandler(filename=os.path.join(log_file_path, log_file_name), mode='a',
-                                  maxBytes=log_size_KB * 1024, backupCount=backup_logs, encoding='utf_8')
-log_handler.setFormatter(log_format)
-log_handler.setLevel(log_level)
-
-get_weather_logging = logging.getLogger('logger')
-get_weather_logging.setLevel(log_level)
-get_weather_logging.addHandler(log_handler)
+from get_weather_logger import get_weather_logger
 
 
 # This function is for encrypting 'appid' when writing the url to the log.
@@ -46,8 +13,11 @@ def encrypt_appid(url_values, api_url_partial):
     return api_url_partial + urllib.parse.urlencode(url_values)
 
 
+get_weather_logging = get_weather_logger()
+
+
 def get_weather_api(location, appid):
-    get_weather_logging.info("----- Start " + str(log_level) + " -----")
+    get_weather_logging.info("----- Start " + str("log_level") + " -----")
     if type(location) is not Location:
         e = GetWeatherException(11, "Location type is mandatory")
         get_weather_logging.critical("exception:", exc_info=e)
